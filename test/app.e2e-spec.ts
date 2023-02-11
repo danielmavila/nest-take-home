e469cba1fd56440aae35e9bfc329949cc433de0d
Daniel Avila <me@danielmavila.com>
2023-02-10 22:58:46 -0700

    Remove Nest's endpoint built with project initialization

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -15,10 +15,9 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/', () => {
     return request(app.getHttpServer())
       .get('/')
-      .expect(200)
-      .expect('Hello World!');
+      .expect(404);
   });
 });
